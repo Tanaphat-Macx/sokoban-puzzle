3ee17ccdb0fc8be0fd5df78f5fce142b7a731c37
CallMeMacx <110654160+Tanaphat-Macx@users.noreply.github.com>
2023-12-05 20:58:55 +0700

    Level 1 - 4

--- Scripts/Test/TileMap.gd
+++ Scripts/Test/TileMap.gd
@@ -1,14 +1,34 @@
 extends TileMap
 
-# ตั้งค่า TileSet ให้กับ TileMap
-var tileset : TileSet
 
+var level = [
+	[1, 1, 1, 1, 1],
+	[1, 0, 0, 0, 1],
+	[1, 0, 0, 0, 1],
+	[1, 0, 0, 0, 1],
+	[1, 1, 1, 1, 1],
+]
+var tile = 64
+#var levelSize = level.size()
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	# ให้ TileMap ใช้ TileSet
-	set_tileset(tileset)
+	Generate_Tilemap()
 
-	# กำหนดแผนที่โดยใช้ตำแหน่งของแต่ละช่องในภาพแผนที่
-	for y in range(get_used_rect().size.y):
-		for x in range(get_used_rect().size.x):
-			var tile_type := get_cell(x, y)
-			set_cell(x, y, tile_type)
+
+func Generate_Tilemap():
+	for y in range(level.size()):
+		for x in range(level[y].size()): #level[y].size()
+			Create_Tile(level[y][x], x, y)
+
+func Create_Tile(tileType, x, y):
+	match tileType:
+		0:
+			set_cell(0, Vector2i(x,y), 0, Vector2i(11,6))
+		1:
+			set_cell(0, Vector2i(x,y), 0, Vector2i(6,7))
+		2:
+			set_cell(1, Vector2i(x,y), 1, Vector2i(1,3))
+		3:
+			set_cell(1, Vector2i(x,y), 1, Vector2i(0,4))
+			
+
